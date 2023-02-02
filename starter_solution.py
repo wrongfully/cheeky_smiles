e17cfba7903a470be7f8f64000394945e12fd208
deandrawhitby <104539455+deandrawhitby@users.noreply.github.com>
2023-02-02 09:51:03 -0600

    Updated solution

---
+++ starter_solution.py
@@ -0,0 +1,34 @@
+def party_invoice():
+    
+    base_price = ...
+    
+    # input each addon as a string with the quantity and type
+    # seperate each addon with a comma
+    addons = input("What are the party addons?")
+    
+    # use the "split" method to convert the addons to a list
+    addons_list = addons.split(",")
+    
+    for addon in addons_list:
+        
+        item = addon[-1]
+        count = addon[:-1]
+        
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    party_invoice()
